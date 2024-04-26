558a4344346d743b9ced696c147eed559993f62e
Marceillo De Wet <marceillo@hotmail.com>
2024-04-26 09:50:09 +0000

    Add requirements for deployment

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
+
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 SCOPE = [
@@ -23,7 +23,8 @@ def get_sales_data():
         print("Data should be six numbers, separated by commas")
         print("Example: 10,20,30,40,50,60\n")
 
-        data_str = input("Enter your data here: ")
+    data_str = input("Enter your data here:\n ")
+    
         sales_data = data_str.split(",")
         validate_data(sales_data)
 
@@ -115,6 +116,20 @@ def get_last_5_entries_sales():
 
     return columns
 
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
 
 
 def main():
@@ -126,8 +141,12 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus" )
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
+    print(stock_data)
       
 
 print("Welcome to love Sandwiches Data Automation")
-# main()
-sales_colums = get_last_5_entries_sales()
+main()
+
